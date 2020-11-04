c202c13df102e6987c87951df2bd4ab6dd41736d
isaac505 <donne1212@hotmail.com>
2020-11-04 15:52:15 -0500

    masge

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+//good day 
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -19,7 +21,7 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
+    box1 = new Box(500,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
